1f5d507269c11caedf61053b69c5e05baa36fb0b
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-02-19 20:49:10 +0000

    Implemented hook for RealURL auto-configuration
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/egovapi/trunk@43892 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,4 +5,7 @@ if (!defined('TYPO3_MODE')) {
 
 t3lib_extMgm::addPItoST43($_EXTKEY, 'Classes/Controller/Pi1/class.tx_egovapi_pi1.php', '_pi1', 'list_type', 1);
 
+// RealURL auto-configuration
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/realurl/class.tx_realurl_autoconfgen.php']['extensionConfiguration'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/Classes/Hooks/RealUrl.php:tx_egovapi_hooks_realurl->addEgovApiConfig';
+
 ?>
