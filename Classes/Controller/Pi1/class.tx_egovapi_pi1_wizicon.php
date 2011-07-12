bf642b73f5521d22f9c736b6d00631ee2377c0b8
xperseguers <xperseguers@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-07-12 18:24:02 +0000

    [BUGFIX] CE wizards for pi1 and pi2 are using deprecated methods in TYPO3 4.6
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/egovapi/branches/EGOVAPI_1-3@49767 735d13b6-9817-0410-8766-e36946ffe9aa

--- Classes/Controller/Pi1/class.tx_egovapi_pi1_wizicon.php
+++ Classes/Controller/Pi1/class.tx_egovapi_pi1_wizicon.php
@@ -59,13 +59,16 @@ class tx_egovapi_pi1_wizicon {
 	 *
 	 * @return The array with language labels
 	 */
-	function includeLocalLang()	{
+	function includeLocalLang() {
 		$llFile = t3lib_extMgm::extPath('egovapi') . 'Resources/Private/Language/locallang.xml';
-		if (t3lib_div::int_from_ver(TYPO3_version) < 4006000) {
+		$version = class_exists('t3lib_utility_VersionNumber')
+				? t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version)
+				: t3lib_div::int_from_ver(TYPO3_version);
+		if ($version < 4006000) {
 			$LOCAL_LANG = t3lib_div::readLLXMLfile($llFile, $GLOBALS['LANG']->lang);
 		} else {
-			/** @var $llxmlParser tx_lang_parser_Llxml */
-			$llxmlParser = t3lib_div::makeInstance('tx_lang_parser_Llxml');
+			/** @var $llxmlParser t3lib_l10n_parser_Llxml */
+			$llxmlParser = t3lib_div::makeInstance('t3lib_l10n_parser_Llxml');
 			$LOCAL_LANG = $llxmlParser->getParsedData($llFile, $GLOBALS['LANG']->lang);
 		}
 
